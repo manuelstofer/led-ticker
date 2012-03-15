b2203e35809279b5710cb07ff2134b92cad83748
Manuel Stofer <mst@rtp.ch>
2012-03-15 08:59:41 +0100

    removes unused file

--- ledticker/protocol.py
+++
@@ -1,9 +0,0 @@
-from twisted.protocols import basic
-from twisted.internet import reactor
-
-class TwistedAdapter(basic.LineReceiver):
-    """
-        Line based adapter for twisted
-    """
-    def lineReceived(self, line):
-        self.factory.queue.add_message(line)
