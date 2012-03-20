16516a3a89901a43a5c838899e95e8702964d156
Manuel Stofer <manuelstofer@gmail.com>
2012-03-20 19:11:00 +0100

    updates server.py to run with -d, --debug option

--- server.py
+++ server.py
@@ -1,20 +1,34 @@
 
+import ledticker
 import time
 import sys
-import ledticker
-
+from optparse import OptionParser
 from twisted.internet import protocol, reactor
 from twisted.application import service, internet
 
-transmitter = ledticker.Transmitter()
 
+# parse options
+parser = OptionParser()
+parser.add_option("-d",     '--debug',
+                  dest      = "debug",
+                  action    = "store_true",
+                  help      = "to use the server without having the device connected you can log to std")
+
+(options, args) = parser.parse_args()
+
+
+## instanciate transmitter
+if not options.debug:
+    transmitter = ledticker.Transmitter() 
+else:
+    transmitter = ledticker.DebugTransmitter()
+
+# setup factory for twisted
 factory = protocol.ServerFactory()
 factory.protocol = ledticker.TwistedAdapter
 factory.queue    = ledticker.MessageQueue(transmitter)
 
+# run in twisted
 reactor.listenTCP(3000,factory)
 reactor.run()
-
-# application = service.Application("tickerserver")
-# internet.TCPServer(3000, factory).setServiceParent(application)
 
